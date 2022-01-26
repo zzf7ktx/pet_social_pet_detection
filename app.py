c9aeb825f25a5f52c0d6b8eef64945f0210da0f5
zzf7ktx <zzf7ktx@gmail.com>
2022-01-26 09:53:04 +0700

    Init

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ if __name__ == '__main__':
         f'Server now running on ')
 
     # starting app
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
